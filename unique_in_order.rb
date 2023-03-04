0e9c48a33e775516055f0ca9a453607dacf66e6b
Yvonne <moluishere@gmail.com>
2023-03-04 20:13:52 +0800

    unique_in_order

---
+++ unique_in_order.rb
@@ -0,0 +1,21 @@
+# DESCRIPTION:
+# Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+#
+# For example:
+#
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+def unique_in_order(iterable)
+  result = (iterable.is_a?(String) ? iterable.split('') : iterable)
+  unique = []
+  result.each_with_index do |item, index|
+    unique << item if index == 0 || item != result[index-1]
+  end
+  unique
+end
+
+puts unique_in_order('AAAABBBCCDAABBB')
+puts unique_in_order('ABBCcAD')
+puts unique_in_order([1,2,2,3,3])
