63ffce9451951bd3eab33eb19eec0ec522862708
Yvonne <moluishere@gmail.com>
2023-03-04 19:59:02 +0800

    Refactor the code by separate methods

--- who_likes_it.rb
+++ who_likes_it.rb
@@ -10,14 +10,32 @@
 # ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 # Note: For 4 or more names, the number in "and 2 others" simply increases.
 
+# def likes(names)
+#   case names.size
+#     when 0 then "no one likes this"
+#     when 1 then"#{names[0]} likes this"
+#     when 2 then "#{names[0]} and #{names[1]} like this"
+#     when 3 then "#{names[0]}, #{names[1]} and #{names[2]} like this"
+#     else"#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+#   end
+# end
+#
 def likes(names)
+  who(names) + ' ' +like_this(names)
+end
+
+def who(names)
   case names.size
-  when 0 then "no one likes this"
-  when 1 then"#{names[0]} likes this"
-  when 2 then "#{names[0]} and #{names[1]} like this"
-  when 3 then "#{names[0]}, #{names[1]} and #{names[2]} like this"
-  else"#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+    when 0 then "no one"
+    when 1 then "#{names[0]}"
+    when 2 then "#{names[0]} and #{names[1]}"
+    when 3 then "#{names[0]}, #{names[1]} and #{names[2]}"
+    else "#{names[0]}, #{names[1]} and #{names.size - 2} others"
   end
+end
+
+def like_this(names)
+  names.size <= 1 ? "likes this" : "like this"
 end
 
 puts likes([])
