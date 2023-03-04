07694e6673f83b3af8483aef8f20bc4179fbcb01
Yvonne <moluishere@gmail.com>
2023-03-04 19:41:52 +0800

    refactor the code more readable

--- who_likes_it.rb
+++ who_likes_it.rb
@@ -12,16 +12,11 @@
 
 def likes(names)
   case names.size
-  when 0
-    "no one likes this"
-  when 1
-    "#{names[0]} likes this"
-  when 2
-    "#{names[0]} and #{names[1]} like this"
-  when 3
-    "#{names[0]}, #{names[1]} and #{names[2]} like this"
-  else
-    "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+  when 0 then "no one likes this"
+  when 1 then"#{names[0]} likes this"
+  when 2 then "#{names[0]} and #{names[1]} like this"
+  when 3 then "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else"#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
   end
 end
 
